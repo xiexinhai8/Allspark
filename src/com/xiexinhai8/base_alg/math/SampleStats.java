bc1f71b68b4ed0581778fbe77144d77467fc19e8
xiexinhai <xiexinhai@kuaishou.com>
2023-05-27 11:49:04 +0800

    1093. 大样本统计

--- src/com/xiexinhai8/base_alg/math/SampleStats.java
+++ src/com/xiexinhai8/base_alg/math/SampleStats.java
@@ -1,6 +1,19 @@
 package com.xiexinhai8.base_alg.math;
 
 /**
+ *
+ * 我们对 0 到 255 之间的整数进行采样，并将结果存储在数组 count 中：count[k] 就是整数 k 在样本中出现的次数。
+ *
+ * 计算以下统计数据:
+ *
+ * minimum ：样本中的最小元素。
+ * maximum ：样品中的最大元素。
+ * mean ：样本的平均值，计算为所有元素的总和除以元素总数。
+ * median ：
+ * 如果样本的元素个数是奇数，那么一旦样本排序后，中位数 median 就是中间的元素。
+ * 如果样本中有偶数个元素，那么中位数median 就是样本排序后中间两个元素的平均值。
+ * mode ：样本中出现次数最多的数字。保众数是 唯一 的。
+ * 以浮点数数组的形式返回样本的统计信息 [minimum, maximum, mean, median, mode] 。与真实答案误差在 10-5 内的答案都可以通过。
  * @author xxh <xiexinhai@kuaishou.com>
  * Created on 2023-05-27
  */
