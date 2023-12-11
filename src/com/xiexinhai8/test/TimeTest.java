5e2f4470a312c42e5020b5d77abc264b432377e8
xiexinhai <xiexinhai@kuaishou.com>
2023-12-11 11:31:45 +0800

    update

--- src/com/xiexinhai8/test/TimeTest.java
+++ src/com/xiexinhai8/test/TimeTest.java
@@ -8,5 +8,8 @@ public class TimeTest {
 
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
+        int x  = (int)1e9;
+        System.out.println(x);
+        System.out.println(Integer.MAX_VALUE);
     }
 }
