a8d6378854f46e1b47ed4a41e281415b48a53312
xiexinhai <xiexinhai@kuaishou.com>
2023-12-12 11:27:35 +0800

    2454. 下一个更大元素 IV

---
+++ src/com/xiexinhai8/base_alg/stack/monotonic/SecondGreaterElement.java
@@ -0,0 +1,27 @@
+package com.xiexinhai8.base_alg.stack.monotonic;
+
+/**
+ * TODO 2454. 下一个更大元素 IV
+ *
+ * 给你一个下标从 0 开始的非负整数数组 nums 。对于 nums 中每一个整数，你必须找到对应元素的 第二大 整数。
+ *
+ * 如果 nums[j] 满足以下条件，那么我们称它为 nums[i] 的 第二大 整数：
+ *
+ * j > i
+ * nums[j] > nums[i]
+ * 恰好存在 一个 k 满足 i < k < j 且 nums[k] > nums[i] 。
+ * 如果不存在 nums[j] ，那么第二大整数为 -1 。
+ *
+ * 比方说，数组 [1, 2, 4, 3] 中，1 的第二大整数是 4 ，2 的第二大整数是 3 ，3 和 4 的第二大整数是 -1 。
+ * 请你返回一个整数数组 answer ，其中 answer[i]是 nums[i] 的第二大整数。
+ *
+ * @author xxh <xiexinhai@kuaishou.com>
+ * Created on 2023-12-12
+ */
+public class SecondGreaterElement {
+
+    public int[] secondGreaterElement(int[] nums) {
+        return null;
+    }
+
+}
