00aa60f8b1008cb08bd37ff99b660fc369f038d5
xiexinhai <xiexinhai@kuaishou.com>
2024-01-09 16:38:44 +0800

    2735. 收集巧克力

--- src/com/xiexinhai8/base_alg/enumeration/MinCost.java
+++ src/com/xiexinhai8/base_alg/enumeration/MinCost.java
@@ -14,8 +14,39 @@ package com.xiexinhai8.base_alg.enumeration;
  * Created on 2024-01-09
  */
 public class MinCost {
-
+    /**
+     * 由于最后一次移动n - 1次的操作和前面移动的操作有重复操作的问题， 可以采取空间换时间的方式来计算每次移动的成本
+     *
+     * 1、定义一个操作成本记录数组
+     * 2、每次操作往对应数组中增加操作成本
+     */
     public long minCost(int[] nums, int x) {
+        long[] cost = new long[nums.length];
+
+        for (int optNum = 0; optNum < nums.length; optNum++) {
+            cost[optNum] += (1L * optNum * x);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int curCost = nums[i];
+            cost[0] += curCost;
+            for (int optNum = 1; optNum < nums.length; optNum++) {
+                curCost = Math.min(curCost, nums[(i + optNum) % nums.length]);
+                cost[optNum] += curCost;
+            }
+        }
+
+        long minCost = cost[0];
+        for (int i = 1; i < nums.length; i++) {
+            minCost = Math.min(minCost, cost[i]);
+        }
+        return minCost;
+    }
+
+    /**
+     * 采取枚举移动次数的方式确定最小的收集成本， 时间复杂度0(n^3)
+     */
+    public long minCost1(int[] nums, int x) {
         long minCost = -1;
 
         for (int i = 0; i < nums.length; i++) {
@@ -28,7 +59,14 @@ public class MinCost {
         return minCost;
     }
 
-    private long calCost(int[] nums, int optNum, int optCost) {
+    /**
+     * 计算固定移动次数的消费
+     * @param nums
+     * @param optNum 操作次数， 必须是long， 否则容易溢出
+     * @param optCost
+     * @return
+     */
+    private long calCost(int[] nums, long optNum, int optCost) {
         long cost = optNum * optCost;
 
         for (int i = 0; i < nums.length; i++) {
