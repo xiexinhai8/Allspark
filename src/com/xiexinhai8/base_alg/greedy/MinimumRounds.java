5f00fef85dc43fb18a04e743f2b0141ea19ca405
xiexinhai8 <xiexinhai@xxh-de-Mac-Pro.local>
2024-05-14 16:40:07 +0800

    2244. 完成所有任务需要的最少轮数

--- src/com/xiexinhai8/base_alg/greedy/MinimumRounds.java
+++ src/com/xiexinhai8/base_alg/greedy/MinimumRounds.java
@@ -5,6 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 2244. 完成所有任务需要的最少轮数
+ *
+ * 给你一个下标从 0 开始的整数数组 tasks ，其中 tasks[i] 表示任务的难度级别。在每一轮中，你可以完成 2 个或者 3 个 相同难度级别 的任务。
+ *
+ * 返回完成所有任务需要的 最少 轮数，如果无法完成所有任务，返回 -1 。
  * @author xxh
  * Created on 2024-05-14
  */
